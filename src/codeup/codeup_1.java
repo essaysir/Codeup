c5010a487f44c39fb8290932c2ee72d42edb8fcf
Son Jusun <sonjs7554@naver.com>
2023-02-02 19:19:48 +0900

    2번 문제

--- src/codeup/codeup_1.java
+++ src/codeup/codeup_1.java
@@ -8,7 +8,13 @@ public class codeup_1 {
 		//Hello
 		System.out.println("Hello");
 		
-		
+		//1002.
+//		이번에는 공백()을 포함한 문장을 출력한다.
+//		다음 문장을 출력해보자.
+//
+//		Hello World
+//		(대소문자에 주의한다.)
+		System.out.println("Hello World");
 		
 		
 		
