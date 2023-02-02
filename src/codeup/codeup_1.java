0416394158a191cc333f2f1cc8febc25006571e7
Son Jusun <sonjs7554@naver.com>
2023-02-02 19:17:51 +0900

    문제1번

--- src/codeup/codeup_1.java
+++ src/codeup/codeup_1.java
@@ -3,9 +3,31 @@ package codeup;
 public class codeup_1 {
 
 	public static void main(String[] args) {
-
-
-	}
+		//1001.
+		//printf()를 이용해 다음 단어를 출력하시오.
+		//Hello
+		System.out.println("Hello");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}// END OF MAIN METHOD ==============================================
 
 }
  
